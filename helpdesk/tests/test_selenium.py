ed80aff377e3c75d596eec7afef6b4e819b5c5e8
pouyansh <pouyan.shirzadian@yahoo.com>
2019-06-25 14:15:00 +0430

    selenium

--- helpdesk/tests/test_selenium.py
+++ helpdesk/tests/test_selenium.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 class SeleniumTests(LiveServerTestCase):
     def setUp(self):
-        self.selenium = webdriver.Firefox(executable_path=r'geckodriver')
+        self.selenium = webdriver.Firefox()
         super(SeleniumTests, self).setUp()
 
     def tearDown(self):
