cb44b97344120eaedbefd1fe06075396e72f1b27
pouyansh <pouyan.shirzadian@yahoo.com>
2019-06-25 14:48:54 +0430

    selenium

--- helpdesk/tests/test_selenium.py
+++ helpdesk/tests/test_selenium.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 class SeleniumTests(LiveServerTestCase):
     def setUp(self):
-        self.selenium = webdriver.Chrome('/usr/local/bin/chromedriver')
+        self.selenium = webdriver.Chrome('/usr/bin/chromedriver')
         super(SeleniumTests, self).setUp()
 
     def tearDown(self):
